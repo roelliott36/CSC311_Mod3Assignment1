87904ededd3c9ac36522f1dd8fef991d8aecfd18
Rochelle Elliott <ro.elliott36@gmail.com>
2026-09-22 21:43:17 -0400

    added the private variables

--- src/main/java/org/example/csc311_mod3assignment1/StartController.java
+++ src/main/java/org/example/csc311_mod3assignment1/StartController.java
@@ -1,14 +1,38 @@
 package org.example.csc311_mod3assignment1;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
 
 public class StartController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TextField rateField;
+
+    @FXML
+    private TextField yearsField;
+
+    @FXML
+    private TextField amountField;
+
+    @FXML
+    private TextField paymentField;
+
+    @FXML
+    private TextField totalField;
+
+    @FXML
+    private TextField rateUsedField;
+
+    @FXML
+    private TextField yearsUsedField;
+
+    @FXML
+    private TextField amountUsedField;
+
+    @FXML
+    private TextField paymentUsedField;
+
+
+
 }
